11154f225d6182295ca38430c38eefd6c4654f57
TatianaArrieta <113394215+TatianaArrieta@users.noreply.github.com>
2022-11-08 20:18:47 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -22,7 +22,7 @@ def pregunta_01():
     40
 
     """
-    return
+    return len(tbl0)
 
 
 def pregunta_02():
@@ -33,7 +33,7 @@ def pregunta_02():
     4
 
     """
-    return
+    return 
 
 
 def pregunta_03():
